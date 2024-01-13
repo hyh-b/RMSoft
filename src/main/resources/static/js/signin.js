c0432ba05518d9136422efef7631cb7a5376acb2
hyh-b <kash4763@naver.com>
2024-01-13 21:11:18 +0900

    RMSoft

--- src/main/resources/static/js/signin.js
+++ src/main/resources/static/js/signin.js
@@ -4,4 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
     if (params.has('fail')) {
         alert('로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.');
     }
+
+    var findId = document.getElementById('findId');
+    var modal = new bootstrap.Modal(document.getElementById('verticalycentered'));
+
+    findId.addEventListener('click', function(event) {
+        event.preventDefault(); // 기본 링크 동작 방지
+        modal.show(); // 모달 열기
+    });
 });
