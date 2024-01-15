864b78ad47e127d5ba6e7bdfa0ebb8c3a85576a5
hyh-b <kash4763@naver.com>
2024-01-15 21:08:49 +0900

    비밀번호 찾기

--- src/main/resources/static/js/signin.js
+++ src/main/resources/static/js/signin.js
@@ -33,58 +33,56 @@ function setupEventListeners() {
     });
 }
 
-function setupAjaxRequests() {
+/* 아이디 찾기 버튼 메서드*/
+function findId() {
+    var email = $('#emailForIdFind').val();
 
-    /* 아이디 찾기 버튼 메서드*/
-    $('#findId').click(function() {
-        var email = $('#emailForIdFind').val();
-
-        $.ajax({
-            url: '/api/signin/findId',
-            type: 'GET',
-            data: { email: email },
-            success: function(data) {
-                var resultDiv = $('#idResult');
-                if (data.length > 0) {
-                    resultDiv.text('회원님의 아이디: ' + data.join(', '));
-                } else {
-                    resultDiv.text('해당 이메일로 등록된 아이디가 없습니다.');
-                }
-            },
-            error: function() {
-                console.log('에러가 발생했습니다.');
+    $.ajax({
+        url: '/api/signin/findId',
+        type: 'GET',
+        data: { email: email },
+        success: function(data) {
+            var resultDiv = $('#idResult');
+            if (data.length > 0) {
+                resultDiv.text('회원님의 아이디: ' + data.join(', '));
+            } else {
+                resultDiv.text('해당 이메일로 등록된 아이디가 없습니다.');
             }
-        });
+        },
+        error: function() {
+            console.log('에러가 발생했습니다.');
+        }
     });
-
-    /* 비밀번호 찾기 버튼 메서드 */
-    $('#findPassword').click(function() {
-        var memberId = $('#idForPasswordFind').val();
-        var email = $('#emailForPasswordFind').val();
-        var resultDiv = $('#passwordResult');
-        showSpinner();
-        $.ajax({
-            url: '/api/signin/findPassword',
-            type: 'Post',
-            data: {
-                memberId: memberId,
-                email: email
-            },
-            success: function(response) {
-                alert(response);
-                hideSpinner();
-            },
-            error: function(xhr, status, error) {
-                alert(xhr.responseText);
-                hideSpinner();
-            }
-        });
-    });
-
 }
+/* 비밀번호 찾기 버튼 메서드*/
+function findPassword() {
+    var memberId = $('#idForPasswordFind').val();
+    var email = $('#emailForPasswordFind').val();
+    showSpinner();
+    $.ajax({
+        url: '/api/signin/findPassword',
+        type: 'Post',
+        data: {
+            memberId: memberId,
+            email: email
+        },
+        success: function(response) {
+            alert(response);
+            hideSpinner();
+        },
+        error: function(xhr, status, error) {
+            alert(xhr.responseText);
+            hideSpinner();
+        }
+    });
+}
+
+$('#findId').click(findId);
+
+$('#findPassword').click(findPassword);
+
 $(document).ready(function() {
     setupEventListeners();
-    setupAjaxRequests();
 
     const params = new URLSearchParams(window.location.search);
     if (params.has('fail')) {
