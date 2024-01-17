0de8b12416f77ebb7f375a90171b3c3124e6e0bb
hyh-b <kash4763@naver.com>
2024-01-17 21:10:35 +0900

    구독 연장 기능

--- src/main/java/com/example/rmsoft/service/SubscriptionExtensionService.java
+++ src/main/java/com/example/rmsoft/service/SubscriptionExtensionService.java
@@ -2,16 +2,26 @@ package com.example.rmsoft.service;
 
 import com.example.rmsoft.dto.SubscriptionExtensionDto;
 import com.example.rmsoft.mapper.SubscriptionExtensionMapper;
+import com.example.rmsoft.mapper.SubscriptionMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @RequiredArgsConstructor
 @Service
 public class SubscriptionExtensionService {
 
     private final SubscriptionExtensionMapper subscriptionExtensionMapper;
+    private final SubscriptionMapper subscriptionMapper;
 
+    @Transactional
     public void subscriptionExtension(SubscriptionExtensionDto subscriptionExtensionDto) {
+        LocalDate extensionEndDate = subscriptionExtensionDto.getExtensionEndDate();
+        int subscriptionCode = subscriptionExtensionDto.getSubscriptionCode();
+
+        subscriptionMapper.updateSubscriptionEndDate(extensionEndDate,subscriptionCode);
         subscriptionExtensionMapper.subscriptionExtension(subscriptionExtensionDto);
     }
 }
--- src/main/java/com/example/rmsoft/web/SubscriptionExtensionController.java
+++ src/main/java/com/example/rmsoft/web/SubscriptionExtensionController.java
@@ -2,11 +2,14 @@ package com.example.rmsoft.web;
 
 import com.example.rmsoft.dto.SubscriptionExtensionDto;
 import com.example.rmsoft.service.SubscriptionExtensionService;
+import com.example.rmsoft.service.SubscriptionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,11 +17,10 @@ public class SubscriptionExtensionController {
 
     private final SubscriptionExtensionService subscriptionExtensionService;
 
+
     @PostMapping("/api/subscription/extension")
-    public ResponseEntity<String> subscriptionExtension(@RequestBody SubscriptionExtensionDto subscriptionExtensionDto) {
-        System.out.println("컨트롤러시작");
-        System.out.println("컨트롤러시작스타트데이"+subscriptionExtensionDto.getExtensionStartDate());
-        System.out.println("컨트롤러dps엔드데이"+subscriptionExtensionDto.getExtensionEndDate());
+    public ResponseEntity<?> subscriptionExtension(@RequestBody SubscriptionExtensionDto subscriptionExtensionDto) {
+
         subscriptionExtensionService.subscriptionExtension(subscriptionExtensionDto);
         return ResponseEntity.ok("연장이 완료되었습니다.");
     }
