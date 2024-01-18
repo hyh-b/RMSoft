1bb91f2484b21e3276d8258a453d89ef22228a2e
hyh-b <kash4763@naver.com>
2024-01-18 15:50:03 +0900

    애플리케이션 한국 시간 설정

--- src/main/java/com/example/rmsoft/RmSoftApplication.java
+++ src/main/java/com/example/rmsoft/RmSoftApplication.java
@@ -1,15 +1,23 @@
 package com.example.rmsoft;
 
+import jakarta.annotation.PostConstruct;
 import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 @MapperScan("com.example.rmsoft.mapper")
 public class RmSoftApplication {
+    // EC2에 배포시 생기는 시간 문제 해결
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RmSoftApplication.class, args);
